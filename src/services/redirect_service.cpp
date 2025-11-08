ea649ecce72d9024f71e2488926b8c65848623b3
Capy <dimak12k@yandex.ru>
2025-11-08 17:10:51 +0300

    implement redirect service methods

--- src/services/redirect_service.cpp
+++ src/services/redirect_service.cpp
@@ -6,12 +6,24 @@ namespace cutr::service {
             : postgresRepo_(std::move(postgresRepo)), redisRepo_(std::move(redisRepo)) {}
 
     drogon::Task<std::optional<std::string> > RedirectService::getOriginalUrl(const std::string &shortCode) {
-        // TODO: Implement this method
-        co_return std::nullopt;
+        auto cachedUrl = co_await redisRepo_->getOriginalUrl(shortCode);
+        if (cachedUrl.has_value()) {
+            co_return cachedUrl;
+        }
+
+        auto url = co_await postgresRepo_->getOriginalUrl(shortCode);
+        if (url.has_value()) {
+            co_await redisRepo_->saveLink(shortCode, *url);
+        }
+
+        co_return url;
     }
 
     drogon::Task<void> RedirectService::incrementHits(const std::string &shortCode) {
-        // TODO: Implement this method
+        co_await postgresRepo_->incrementHits(shortCode);
+
+        co_await redisRepo_->incrementHits(shortCode);
+
         co_return;
     }
 
