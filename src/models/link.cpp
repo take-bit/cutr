e216400421f757d6cd962ae99b188d08fd7bf6f7
Capy <dimak12k@yandex.ru>
2025-11-08 17:35:48 +0300

    remove unused model files

--- src/models/link.cpp
+++
@@ -1,6 +0,0 @@
-#include <models/link.h>
-
-model::Link::Link() = default;
-
-model::Link::Link(std::string sc, std::string url)
-        : shortCode(std::move(sc)), originalUrl(std::move(url)), createdAt(std::chrono::system_clock::now()) {}
